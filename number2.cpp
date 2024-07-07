47ea1783aabcbf2695abca7c5d9131fe5f97adba
noor osama <lanoor64@gmail.com>
2024-07-07 19:39:58 +0300

    Update number2.cpp

--- number2.cpp
+++ number2.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main ()
 {
-
-cout<<" Hello world ";
+ int i= 2, j= 3, m;
+    m = i++ + j;
+    cout<<"m = \n" <<m;
+    cout<< "\n" << i;
+    // (i) will be 3 
 
     return 0;
 }
