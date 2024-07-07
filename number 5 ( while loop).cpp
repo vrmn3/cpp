b3c33cf4dfb8e0d187008713bf38062bf7487a3b
noor osama <lanoor64@gmail.com>
2024-07-08 00:35:11 +0300

    Add files via upload

---
+++ number 5 ( while loop).cpp
@@ -0,0 +1,18 @@
+//this program to print the sum of numbers entered from the user as long as they are positive.
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+int numbers , sum =0;
+while ( number >= 0)
+{
+cout <<" enter positive number \n";
+cin >> number;
+
+sum += number;0
+}
+
+cout <<" the sum of numbers = \n"<< sum;
+    return 0;
+}
