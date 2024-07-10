5a02dbfb2615350e7e5ac5aa0a6d33832de71ee5
noor osama <lanoor64@gmail.com>
2024-07-10 22:17:11 +0300

    Add files via upload

---
+++ number 8 ( functions )/sphere calculation.cpp
@@ -0,0 +1,31 @@
+/*
+This program is to calculate the Area and volume for a sphere.
+- The area of sphere = 4 * PI * Radius * Radius.
+- The Volume of sphere = 3/4 * PI * Radius * Radius * Radius.
+- Note: Pl =3.14
+*/
+#include <iostream> 
+#include <iostream>
+using namespace std;
+
+float area ( int  );
+float volume ( int );
+int main()
+{
+  int radius ;
+  cout<<" enter the radius \n ";
+  cin>> radius ;
+  cout<<" the area of sphere = \n " << area (  radius ) << "\n";
+  cout<<" the volume of sphere = \n " << volume (  radius ) << "\n";
+  return 0;
+}
+
+float area ( int r ) 
+{
+    return 4 * 3.14 * r * r ;
+}
+
+float volume ( int r )
+{
+    return 3/4 * 3.14 * r * r * r ;
+}
